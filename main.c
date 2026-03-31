3cd335b73e947eaf8ad2f9c8cd2cfc786a915919
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-03-31 20:07:18 +0200

    commited

--- main.c
+++ main.c
@@ -6,12 +6,20 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:50:09 by rokuni            #+#    #+#             */
-/*   Updated: 2026/03/31 19:50:17 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/03/31 20:07:05 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <unistd.h>
 
-int bytes_read;
-
-bytes_read = read(fd, cup_buffer, 3);
+char *get_next_line(int fd)
+{
+static char *stash; // NULL at program start (automatically)
+// ...
+// Call 1: stash is NULL, we read, stash becomes 'Hello\nWo'
+// We return 'Hello\n', stash becomes 'Wo'
+// Call 2: stash is 'Wo' (still here!)
+// We keep reading, stash becomes 'World\n'
+// We return 'World\n', stash becomes ''
+// ...
+}
