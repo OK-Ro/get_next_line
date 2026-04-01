9bfbe3bbdbe562bc57000905dac0346e819aef00
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-01 16:34:59 +0200

    new

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:50:09 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/01 16:26:28 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/01 16:30:08 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,65 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+int get_next_line(int fd, char **line)
+{
+    static char buffer[1024];
+    static int buffer_index = 0;
+    static int bytes_read = 0;
+    char *temp_line = NULL;
+    int line_length = 0;
+
+    if (fd < 0 || !line)
+        return (-1);
+
+    while (1)
+    {
+        if (buffer_index >= bytes_read)
+        {
+            bytes_read = read(fd, buffer, sizeof(buffer));
+            buffer_index = 0;
+            if (bytes_read <= 0)
+                break;
+        }
+
+        char c = buffer[buffer_index++];
+        temp_line = realloc(temp_line, line_length + 2);
+        temp_line[line_length++] = c;
+        temp_line[line_length] = '\0';
+
+        if (c == '\n')
+            break;
+    }
+
+    if (line_length == 0)
+    {
+        free(temp_line);
+        return (0);
+    }
+
+    *line = temp_line;
+    return (1);
+}   
+
 int main(void)
 {
-    int fd = open("new.txt", O_RDONLY);
+    int fd; 
+    char *line;
+    
+    fd = open("test.txt", O_RDONLY);
     if (fd == -1)
     {
         perror("Error opening file");
         return (1);
     }
-    char buffer[100];
-    ssize_t bytesRead = read(fd, buffer, sizeof(buffer) - 1);
-    if (bytesRead == -1)
+        
+    while (get_next_line(fd, &line) > 0)
     {
-        perror("Error reading file");
-        close(fd);
-        return (1);
+        printf("%s\n", line);
+        free(line);
     }
-    buffer[bytesRead] = '\0'; // Null-terminate the buffer
-    printf("File contents:\n%s\n", buffer);
+    free(line);
     close(fd);
-    
     return (0);
 }
