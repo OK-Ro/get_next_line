5664f0e44ad4654ecaed7c280324e6d43340fef9
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-01 14:48:13 +0200

    next

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:50:09 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/01 14:36:11 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/01 14:47:02 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,36 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-
+// add next line to compile with C17 standard
 int main(void)
 {
-    int fd = open("new.txt", O_RDONLY);
-    if (fd == -1)
-    {
-        printf("Error opening file\n");
-        return (1);
+    int fd;
+    char buffer[100];
+
+    // Open the file for reading
+    fd = open("example.txt", O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
     }
 
-    char *buffer = malloc(100);
-    int bytes_read;
-	int i = 1;
+    // Read from the file
+    ssize_t bytesRead = read(fd, buffer, sizeof(buffer) - 1);
+    if (bytesRead == -1) {
+        perror("Error reading file");
+        close(fd);
+        return EXIT_FAILURE;
+    }
 
-	while ((bytes_read = read(fd, buffer, 10)) > 0)
-	{
-    	printf("Call #%d: read %d bytes → \"", i++, bytes_read);
-   	 	write(1, buffer, bytes_read);
-    	printf("\"\n");
-	}
+    // Null-terminate the buffer
+    buffer[bytesRead] = '\0';
+
+    // Print the contents of the file
+    printf("File contents:\n%s\n", buffer);
+
+    // Close the file descriptor
     close(fd);
-    return (0);
+    
+    return EXIT_SUCCESS;
 }
 
