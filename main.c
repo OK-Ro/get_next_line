2bf89f6314b4a7abe4b6ba39477842a790351c2d
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-01 12:34:14 +0200

    pratice

--- main.c
+++ main.c
@@ -6,20 +6,44 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:50:09 by rokuni            #+#    #+#             */
-/*   Updated: 2026/03/31 20:07:05 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/01 12:33:55 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-char *get_next_line(int fd)
+int main(void)
 {
-static char *stash; // NULL at program start (automatically)
-// ...
-// Call 1: stash is NULL, we read, stash becomes 'Hello\nWo'
-// We return 'Hello\n', stash becomes 'Wo'
-// Call 2: stash is 'Wo' (still here!)
-// We keep reading, stash becomes 'World\n'
-// We return 'World\n', stash becomes ''
-// ...
+    // Step 1: Understanding file descriptors and basic reading
+    // Let's start by reading from a file character by character
+
+    int fd = open("test.txt", O_RDONLY);
+	
+    if (fd == -1)
+    {
+        printf("Error opening file\n");
+        return (1);
+    }
+	
+    char buffer[1];
+    int bytes_read;
+
+    printf("Reading file character by character:\n");
+    while ((bytes_read = read(fd, buffer, 1)) > 0)
+    {
+        printf("%c", buffer[1]);
+    }
+
+    close(fd);
+    printf("\n\nNow you understand basic file reading!\n");
+    printf("Key concepts:\n");
+    printf("- File descriptors (fd) are integers that represent open files\n");
+    printf("- read() returns number of bytes read, 0 at EOF, -1 on error\n");
+    printf("- We need to close files when done\n");
+
+    return (0);
 }
+
