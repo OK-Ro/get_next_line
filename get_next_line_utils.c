83f3c31ac850ca399dd87fa725f969b7c0bdda41
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-01 17:59:10 +0200

    new

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,16 +6,48 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:51:57 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/01 16:58:02 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/01 17:58:35 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s);
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+	if (!s)
+		return (0);
+	len = 0;
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+// find first occurrence of c in s
+// return pointer to it or NULL if not found
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+	
+	unsigned char *ps;
+	ps = (unsigned char *)s;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((unsigned char*)ps);
+		i++;
+	}
+	if (s[i] == '\0')
+		return ((unsigned char*)ps);
+	return (NULL);
+}
 
-char	*ft_strchr(const char *s, int c);
-
+// join s1 and s2 into a new string
+// return the new combined string
 char	*ft_strjoin(char const *s1, char const *s2);
 
+// create a duplicate of s1
+// return the new copied string
 char	*ft_strdup(const char *s1);
