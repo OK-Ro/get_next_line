81a95e350cb164496e68c862b01bb2be71219c15
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-02 13:44:17 +0200

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:51:48 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/02 13:42:28 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/02 13:44:12 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,23 @@
 #include <fcntl.h>
 
 
+static char	*read_line(int fd, char *buffer)
+{
+	char	*temp_buffer;
+	char	read_buffer[BUFFER_SIZE + 1];
+	ssize_t	bytes_read;
+
+	while (!ft_strchr(buffer, '\n'))
+	{
+		bytes_read = read(fd, read_buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (NULL);
+		if (bytes_read == 0)
+			break ;
+		read_buffer[bytes_read] = '\0';
+		temp_buffer = buffer;
+		buffer = ft_strjoin(buffer, read_buffer);
+		free(temp_buffer);
+	}
+	return (buffer);
+}
