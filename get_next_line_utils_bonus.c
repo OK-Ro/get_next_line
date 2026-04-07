a22ea7cd4c39c95a9061a1b9950389e7e1443ec4
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-07 17:29:10 +0200

    new

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:19:37 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/07 17:27:22 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/07 17:29:06 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,29 +42,11 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t				i;
-	unsigned char		*d;
-	const unsigned char	*s;
-
-	if (!dest && !src)
-		return (NULL);
-	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	return (dest);
-}
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*newstring;
-	size_t	len1;
-	size_t	len2;
+	int		i;
+	int		j;
 
 	if (!s1 && !s2)
 		return (NULL);
@@ -72,14 +54,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	newstring = malloc(len1 + len2 + 1);
+	newstring = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!newstring)
 		return (NULL);
-	ft_memcpy(newstring, s1, len1);
-	ft_memcpy(newstring + len1, s2, len2);
-	newstring[len1 + len2] = '\0';
+	i = 0;
+	while (s1[i])
+	{
+		newstring[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		newstring[i++] = s2[j++];
+	newstring[i] = '\0';
 	return (newstring);
 }
 
