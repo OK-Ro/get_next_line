46c4fe2b8596df6ab721844d82f6cb37ad9efef6
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-07 11:21:10 +0200

    new

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:19:37 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/07 11:20:40 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/07 11:21:05 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,4 +40,32 @@ char	*ft_strchr(const char *s, int c)
 	if ((char)c == '\0')
 		return ((char *)s);
 	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*newstring;
+	int		i;
+	int		j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	newstring = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!newstring)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		newstring[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		newstring[i++] = s2[j++];
+	newstring[i] = '\0';
+	return (newstring);
 }
