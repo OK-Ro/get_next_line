8b6f4804b54e7727d1be446f56647e3471dc58dc
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-07 11:20:44 +0200

    new

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:19:37 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/07 11:20:15 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/07 11:20:40 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,19 @@ size_t	ft_strlen(const char *s)
 		s++;
 	}
 	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
