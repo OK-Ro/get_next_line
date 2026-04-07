82b1b4bfafb447607832e2eff701081c6e24c8aa
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-07 11:21:50 +0200

    new

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 11:19:37 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/07 11:21:25 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/07 11:21:42 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,4 +90,30 @@ char	*ft_strdup(const char *s1)
 	}
 	duplicate[i] = '\0';
 	return (duplicate);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	s_len;
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = malloc((len + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
