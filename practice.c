ca8ea9557910cbba18ce6c415c4bcb4d6d799fe2
o.robert1994@hotmail.com <rokuni@f1r4s14.codam.nl>
2026-04-08 13:07:34 +0200

    commited

--- practice.c
+++ practice.c
@@ -1,6 +1,30 @@
 #include "get_next_line_bonus.h"
 
+static char extract_line(char **leftover)
+{
+	char *newposition;
+	char *temp;
+	char *line;
 
+	if (!leftover || !**leftover == '\0')
+		return (NULL);
+	newposition = ft_strchr(*leftover, '\n');
+	if (newposition)
+	{
+		line = ft_substr(*leftover, 0, newposition - *leftover + 1);
+		if (!line)
+			return (free(*leftover), *leftover = NULL, NULL);
+		if (*(newposition + 1))
+			temp = ft_strdup(leftover);
+		else
+			temp = NULL;
+		return (free(*leftover), *leftover = temp, line);
+	}
+	line = ft_strdup(leftover);
+	free(leftover);
+	leftover = NULL;
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
