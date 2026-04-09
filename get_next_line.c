f64c5aa5dd5727014acbe863a641179061044000
o.robert1994@hotmail.com <rokuni@f0r1s8.codam.nl>
2026-04-09 11:51:55 +0200

    prac

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:51:48 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/09 11:51:42 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/09 11:51:53 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,5 @@ static char	*extract_line(char **leftover)
 
 char	*get_next_line(int fd)
 {
-	static char	*leftover;
-	ssize_t read_bytes;
-	char *temp;
-	char buffer[BUFFER_SIZE + 1];
-	
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	while (read_bytes > 0 && (!leftover || !ft_strchr(leftover, '\n')))
-	{
-		
-	}
-	return (extract_line());
+	staticchar
 }
