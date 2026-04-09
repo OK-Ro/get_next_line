f60a9f3e78931a61071086146dbc909790f35474
o.robert1994@hotmail.com <rokuni@f0r1s8.codam.nl>
2026-04-09 11:49:39 +0200

    prac

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:51:48 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/09 11:48:42 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/09 11:49:34 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static char	*extract_line(char **leftover)
 	char	*paragragh;
 	char	*rest;
 	
-	if (!leftover || **leftover == '\0')
+	if (!leftover || !**leftover == '\0')
 		return (free(*leftover), *leftover = NULL, NULL);
 	line_found = ft_strchr(*leftover, '\n');
 	if(line_found)
