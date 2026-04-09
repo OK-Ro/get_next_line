26404230dbf33fc5d898c09d501fbe966f0b23ad
o.robert1994@hotmail.com <rokuni@f0r1s8.codam.nl>
2026-04-09 11:49:25 +0200

    prac

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rokuni <rokuni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:51:48 by rokuni            #+#    #+#             */
-/*   Updated: 2026/04/08 12:39:47 by rokuni           ###   ########.fr       */
+/*   Updated: 2026/04/09 11:48:42 by rokuni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,50 +14,51 @@
 
 static char	*extract_line(char **leftover)
 {
-	char	*new_position;
-	char	*temp;
-	char	*line;
-
-	if (!*leftover || **leftover == '\0')
+	char	*line_found;
+	char	*paragragh;
+	char	*rest;
+	
+	if (!leftover || **leftover == '\0')
 		return (free(*leftover), *leftover = NULL, NULL);
-	new_position = ft_strchr(*leftover, '\n');
-	if (new_position)
+	line_found = ft_strchr(*leftover, '\n');
+	if(line_found)
 	{
-		line = ft_substr(*leftover, 0, new_position - *leftover + 1);
-		if (!line)
+		paragragh = ft_substr(*leftover, 0, line_found - *leftover + 1);
+		if (!paragragh)
 			return (free(*leftover), *leftover = NULL, NULL);
-		if (*(new_position + 1))
-			temp = ft_strdup(new_position + 1);
+		if (*(line_found + 1))
+			rest = ft_strdup(*leftover);
 		else
-			temp = NULL;
-		return (free(*leftover), *leftover = temp, line);
+			rest = 	NULL;
+		return (free(*leftover), *leftover = rest, paragragh);
 	}
-	line = ft_strdup(*leftover);
-	free(*leftover);
+	paragragh = ft_strdup(*leftover);
+	free (*leftover);
 	*leftover = NULL;
-	return (line);
+	return (paragragh);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*leftover;
-	ssize_t		read_bytes;
-	char		*temp;
-	char		buffer[BUFFER_SIZE + 1];
-
+	ssize_t read_bytes;
+	char *temp;
+	char buffer[BUFFER_SIZE + 1];
+	
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	read_bytes = 1;
-	while (read_bytes > 0  && (!leftover || !ft_strchr(leftover, '\n')))
+	while (read_bytes > 0 && (!leftover || !ft_strchr(leftover, '\n')))
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (read_bytes > 0)
 		{
-		buffer[read_bytes] = '\0';
-		temp = ft_strjoin(leftover, buffer);
-		free(leftover);
-		leftover = temp;
+			buffer[read_bytes] = '\0';
+			temp = ft_strjoin(leftover, buffer);
+			free(leftover);
+			leftover = temp;
 		}
+		
 	}
 	if (read_bytes < 0)
 		return (free(leftover), leftover = NULL, NULL);
